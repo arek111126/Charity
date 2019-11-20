02cffb8ab5ceabdb99590979c9c223837f6774d5
arek111126 <arek111126@gmail.com>
2019-11-20 02:52:12 +0100

    #7 implement delete institution feauture

--- src/main/java/pl/coderslab/charity/controller/AdminController.java
+++ src/main/java/pl/coderslab/charity/controller/AdminController.java
@@ -1,17 +1,43 @@
 package pl.coderslab.charity.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.charity.entity.Category;
+import pl.coderslab.charity.entity.Institution;
+import pl.coderslab.charity.service.InstitutionService;
+
+import javax.swing.*;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    InstitutionService institutionService;
+
+    @ModelAttribute("institutions")
+    public List<Institution> getAllCategories() {
+        return institutionService.findAll();
+    }
+
     @GetMapping("/dashboard")
     public String adminPage() {
         return "adminTemplates/index";
+    }
+
+    @GetMapping("/institutions")
+    public String getInstitutions(Model model) {
+        return "adminTemplates/tables";
+    }
+
+    @PostMapping("/institution/{id}/delete")
+    public String deleteInstitution(@PathVariable int id, Model model) {
+        institutionService.delete(institutionService.findById(id));
+        model.addAttribute("institutions", institutionService.findAll());
+        return "adminTemplates/tables";
     }
 }
